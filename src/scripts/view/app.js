94b5416379307c7790fdcda9f2a63b4af4aacc01
Rdn016 <m.ridhwankurniawan@gmail.com>
2024-11-25 18:47:17 +0700

    update aksesibilitas

--- src/scripts/view/app.js
+++ src/scripts/view/app.js
@@ -3,18 +3,24 @@ import urlParser from '../routes/url-parser';
 
 class App {
   constructor({ content }) {
-    this._content = content; // Elemen DOM target
+    this._content = content;
   }
 
   async renderPage() {
     const url = urlParser.parseActiveUrlWithCombiner();
     const page = routes[url];
 
-    // Sisipkan HTML ke dalam elemen target
     this._content.innerHTML = await page.render();
 
-    // Panggil afterRender setelah konten masuk ke DOM
     await page.afterRender();
+    const skipButton = document.querySelector('#skip-button');
+    const mainContent = document.querySelector('#mainContent');
+
+    skipButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      mainContent.scrollIntoView({ behavior: 'smooth' });
+      skipButton.blur();
+    });
   }
 }
 
