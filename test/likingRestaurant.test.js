c4ca7944a72da5295dc8f28e965ffab44ce573b9
Rdn016 <m.ridhwankurniawan@gmail.com>
2024-12-16 18:14:33 +0700

    problem di bagian masukin data ke idb

--- test/likingRestaurant.test.js
+++ test/likingRestaurant.test.js
@@ -3,24 +3,38 @@ import favoriteRestaurantidb from '../src/global/liked-restaurant';
 import { createLikeButtonPresenterWithRestaurant } from './helper/testFactories';
 
 describe('like restaurant', () => {
-  describe('liking a restaurant', ()=>{
-    const addLikeButtonContainer = () =>  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
 
-    beforeEach(() => {
-      addLikeButtonContainer();
+  const addLikeButtonContainer = () =>  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+
+  afterEach(async () => {
+    (await favoriteRestaurantidb.getAllRestaurants()).forEach(async (restaurant) => {
+      await favoriteRestaurantidb.deleteRestaurant(restaurant.id);
     });
+  });
 
-    afterEach(async () => {
-      (await favoriteRestaurantidb.getAllRestaurants()).forEach(async (restaurant) => {
-        await favoriteRestaurantidb.deleteRestaurant(restaurant.id);
-      });
-    });
-
+  describe('liking a restaurant',  ()=>{
     it('should show like button when some restaurant has not been liked before', async () => {
       await createLikeButtonPresenterWithRestaurant({ id: 1 });
 
       expect(document.querySelector('[aria-label="like this restaurant"]')).toBeTruthy();
     });
+
+    it('should be able to like a restaurant', async () => {
+      await createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+      const likeButton = document.querySelector('#likeBtn');
+
+      likeButton.dispatchEvent(new Event('click'));
+      const restaurant = await favoriteRestaurantidb.getRestaurant(1);
+      console.log(restaurant);
+      expect(restaurant).toEqual([]);
+    });
+
+
   });
   describe('unliking a restaurant', ()=>{
 
