249ec9a63ddf884a7774de8a7fdce5af36cedcb5
Rdn016 <m.ridhwankurniawan@gmail.com>
2025-03-15 00:47:42 +0700

    hapus skeleton

--- src/scripts/components/Card.jsx
+++ src/scripts/components/Card.jsx
@@ -4,20 +4,17 @@ import { Link } from "react-router-dom";
 const Card = ({ restaurant }) => {
   const { id, city, rating, name, pictureId } = restaurant;
   return (
-    <div className="card lazyload" tabIndex="0" id={id}>
-      <div className="card-head lazyload">
+    <div className="card " tabIndex="0" id={id}>
+      <div className="card-head ">
         <p id="city">{city}</p>
         <p id="rating">★ {rating}</p>
       </div>
-      <div className="skeleton-image"></div>
       <img
-        className="lazyload"
-        data-src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
+        src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
         alt={`restoran ${name}`}
         crossOrigin="anonymous"
-        style={{ display: "none" }}
       />
-      <div className="card-title lazyload">
+      <div className="card-title ">
         <Link to={`/detail/${id}`}>{name}</Link>
       </div>
     </div>
