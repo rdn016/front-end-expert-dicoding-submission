3edff876b83c99cccc3d3e8b13cdb0406ab30916
Rdn016 <m.ridhwankurniawan@gmail.com>
2025-04-16 17:21:35 +0700

    pecah styling jadi lebih kecil biar enak dibaca

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import App from "./scripts/app";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import './styles/style.css'
+import './styles/main.css'
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
