2e1bacc1729565a9fda76fad91b3e26344b74f43
Rdn016 <m.ridhwankurniawan@gmail.com>
2025-04-17 20:35:40 +0700

    ambil username dari token

---
+++ src/scripts/utils/auth.js
@@ -0,0 +1,11 @@
+export const getUsernameFromToken = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload.username;
+  } catch {
+    return null;
+  }
+};
